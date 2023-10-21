efb6555010d589d009a4e0dd1d0fd14ba81c0c80
jimmy90s <jimmy90s@outlook.com>
2023-10-21 13:06:00 -0700

    build changes

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -22,7 +22,7 @@ export default function Home() {
       <section className="grid grid-cols-1 lg:grid-cols-[2fr_1fr] gap-4 mb-4">
         <div className="bg-zinc-200 h-[30rem] rounded-3xl p-10 flex flex-col gap-16 bg-[url('/gradient-bg.jpg')] bg-cover">
           <h1 className="text-4xl font-semibold">
-            Hello, I'm a Product Designer with 7 years of experience.
+            Hello, Im a Product Designer with 7 years of experience.
           </h1>
           <p className="flex-1">
             I care a lot about using design for positive impact and enjoy
