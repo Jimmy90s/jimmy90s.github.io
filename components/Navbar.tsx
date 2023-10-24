6edb5e380d1d16464f0ea6ff125db208f0d6691e
jimmy90s <jimmy90s@outlook.com>
2023-10-23 21:12:15 -0700

    Made Header compnent more mobile friendly

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -9,7 +9,7 @@ const Navbar = () => {
   }, []);
 
   return (
-    <nav className="flex justify-between items-center py-4 px-2">
+    <nav className="flex justify-evenly lg:justify-between items-center py-4 px-2">
       <div className="flex items-center gap-[1ch]">
         <Link className="w-5 h-5 bg-yellow-400 rounded-full" href="#" />
         <Link className="text-sm font-semibold tracking-widest" href="#">
@@ -24,7 +24,7 @@ const Navbar = () => {
             target="_blank"
             rel="noopener noreferrer"
           >
-            <button className="bg-black text-white font-medium py-3 px-12 rounded-full w-60 lg:w-auto hover:bg-instagram dark:bg-white dark:text-black">
+            <button className="bg-black text-white font-medium py-3 px-12 rounded-full w-30 lg:w-auto hover:bg-instagram dark:bg-white dark:text-black">
               Resume
             </button>
           </Link>
